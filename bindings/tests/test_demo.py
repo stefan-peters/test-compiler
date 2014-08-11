03eb6ea7b904e7a82cd731a00752b5308c42e1cb
Stefan Peters <baumkronenprinz@googlemail.com>
2014-08-11 07:11:08 +0200

    completed the py tests

--- bindings/tests/test_demo.py
+++ bindings/tests/test_demo.py
@@ -1,3 +1,15 @@
 import cmc.spam
+import cmc.hello
 
 #cmc.spam.system("ls -la")
+
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+	def test_me(self):
+		self.assertEqual(cmc.hello.greet(), "hello, world")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
