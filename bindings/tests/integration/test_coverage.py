8bf528940adcfdf834c390b11f4b190db001018c
Stefan Peters <baumkronenprinz@googlemail.com>
2014-12-06 11:18:19 +0100

    added (dummy) integration tests and refactored the project structure

---
+++ bindings/tests/integration/test_coverage.py
@@ -0,0 +1,13 @@
+import os
+
+
+def which(tool):
+    for path in os.environ['PATH'].split(os.pathsep):
+        p = os.path.join(path, tool)
+        if os.path.exists(p):
+            return os.path.abspath(p)
+    return None
+
+
+def test_tool_works():
+    assert which("coverage")
