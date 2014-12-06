1e7ef0fd17352dec2fa7e32cfe561cb9836410ec
Stefan Peters <baumkronenprinz@googlemail.com>
2014-12-06 23:54:34 +0100

    added documentation

--- bindings/tests/integration/test_coverage.py
+++ bindings/tests/integration/test_coverage.py
@@ -2,6 +2,7 @@ import os
 
 
 def which(tool):
+    """ return the absolute path to the tool """
     for path in os.environ['PATH'].split(os.pathsep):
         p = os.path.join(path, tool)
         if os.path.exists(p):
